dbf083076a094d6851887abeae7b59fa79109f9b
veera83372 <veeravenky222@gmail.com>
2018-02-16 11:56:00 +0530

    [#6] Aditi/Veera - Give 6 tiles to each player.
    What changes have you made?
    
    * Added tileBox model with following methods
        * getNTiles
        * generateTiles
    
    Why do we need this change?
    
    *  To generate and distribute tiles
    
    What can this change impact?
    * No impact

---
+++ src/models/tileBox.js
@@ -0,0 +1,22 @@
+class TileBox {
+  constructor(cols, rows) {
+    this.cols = cols;
+    this.rows = rows;
+    this.tiles=this.generateTiles();
+  }
+  generateTiles() {
+    let tiles=[];
+    for (let rowIndex = 0; rowIndex < this.rows; rowIndex++) {
+      for (let colIndex = 1; colIndex <= this.cols; colIndex++) {
+        let tile=`${colIndex}${String.fromCharCode(65+rowIndex)}`;
+        tiles.push(tile);
+      }
+    }
+    return tiles;
+  }
+  getNTiles(noOfTiles) {
+    let nTiles=this.tiles.splice(0,noOfTiles);
+    return nTiles;
+  }
+}
+module.exports=TileBox;
---
+++ test/models/tileBoxTest.js
@@ -0,0 +1,21 @@
+const assert = require('chai').assert;
+const TileBox = require('../../src/models/tileBox.js');
+
+describe('TileBox', () => {
+  describe('getNTiles',()=>{
+    it('should return specified n number of tiles ',()=>{
+    let tileBox=new TileBox(12,9);
+    let firstTile=tileBox.tiles[0];
+    let actual=tileBox.getNTiles(6);
+    assert.equal(actual.length,6);
+    assert.deepEqual(actual[0],firstTile);
+    });
+  });
+  describe('generateTiles',()=>{
+    it('should generate all tiles',()=>{
+    let tileBox=new TileBox(1,1);
+    let expected=['1A'];
+    assert.deepEqual(tileBox.generateTiles(),expected);
+    });
+  });
+});
